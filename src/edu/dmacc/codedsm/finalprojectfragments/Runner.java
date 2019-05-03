91f86daa839c2d496d829ecfdff863a4b21eb463
John Cross <jwcross1@dmacc.edu>
2019-05-03 08:52:42 -0500

    working on adding to hashmap and printing to console

--- src/edu/dmacc/codedsm/finalprojectfragments/Runner.java
+++ src/edu/dmacc/codedsm/finalprojectfragments/Runner.java
@@ -4,40 +4,64 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Runner {
 
     public static void main(String[] args) throws IOException {
+        addToHashMap();
+        promptToDisplay();
 
-        readFile();
-
-        DisplayEmployeeInfo displayEmployeeInfo = new DisplayEmployeeInfo();
-
-        System.out.println(displayEmployeeInfo);
 
     }
 
-    public static void readFile() {
+    public static void addToHashMap() {
         HashMap<Integer, String> hmap = new HashMap<>();
-
-        //Name of the file
         String fileName = "initial_load.txt";
         try {
-            //Create object of FileReader
             FileReader inputFile = new FileReader(fileName);
-            //Instantiate the BufferedReader Class
             BufferedReader bufferReader = new BufferedReader(inputFile);
-            //Variable to hold the one line data
             String line;
-            // Read file line by line and print on the console
             while ((line = bufferReader.readLine()) != null) {
                 Integer firstChar = line.indexOf(0);
                 String restOfString = line.substring(1);
                 hmap.put(firstChar, restOfString);
+                System.out.println(hmap);
             }
-            //Close the buffer readerpu
+
             bufferReader.close();
-            } catch (Exception e) {
+        } catch (Exception e) {
+        }
+    }
+
+    public static void promptToDisplay() {
+        boolean isRunning = true;
+        Scanner scanner = new Scanner(System.in);
+        while (isRunning) {
+            System.out.println("Press 1 to list all employees. Press 2 to exit program.");
+            String listEmployeesOrNot = scanner.next();
+            {
+                if (listEmployeesOrNot.equals("1")) {
+                    String fileName = "initial_load.txt";
+                    try {
+                        FileReader inputFile = new FileReader(fileName);
+                        BufferedReader bufferReader = new BufferedReader(inputFile);
+                        String line;
+                        while ((line = bufferReader.readLine()) != null) {
+                            System.out.println("Data is: " + line + ", 0");
+                        }
+                        bufferReader.close();
+                    } catch (Exception e) {
+                        System.out.println("Error while reading file line by line:"
+                                + e.getMessage());
+                    }
+                    isRunning = false;
+                } else if (listEmployeesOrNot.equals("2")) {
+                    isRunning = false;
+                } else isRunning = true;
+            }
         }
     }
 }
+
+
