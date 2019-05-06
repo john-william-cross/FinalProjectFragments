8fd6758d43d8338d17244d9c46479da3aaaeaf50
John Cross <jwcross1@dmacc.edu>
2019-05-06 10:59:41 -0500

    added MVC structure classes

---
+++ src/edu/dmacc/codedsm/finalprojectfragments/Employee.java
@@ -0,0 +1,40 @@
+package edu.dmacc.codedsm.finalprojectfragments;
+
+public class Employee {
+    private String idNumber;
+    private String name;
+    private String hourlyRate;
+    private String hoursWorked;
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(String hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public String getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(String hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+}
